0caf15625be97f4f4bfa21e2bc5eb2e1b96e06bd
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-05 10:51:08 -0500

    Finished 15th lesson: magic methods __get, __set, __isset, __unset

---
+++ magic/public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Styde\User;
+
+require '../vendor/autoload.php';
+
+$user = new User();
+
+$user->fill([
+	'first_name' => 'Johan',
+	'last_name'  => 'Quiroga',
+]);
+
+$user->nickname = 'johanquiroga';
+
+unset($user->nickname);
+
+echo "<p>Bienvenido {$user->first_name} {$user->last_name}</p>";
+
+if (isset($user->nickname)) {
+	echo "<p>Nickname: {$user->nickname}</p>";
+}
